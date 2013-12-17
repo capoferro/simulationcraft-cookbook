09a7e30d24749b334e77812b00a0f33fc466fb19
Josiah Kiehl <jkiehl@riotgames.com>
2013-12-16 23:07:31 -0800

    Add default attrs

---
+++ attributes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: simulationcraft
+# Author:: Josiah Kiehl <(josiah@skirmisher.net)>
+#
+# Copyright 2013, Skirmisher Software
+#
+# License:: MIT
+#
+
+default[:simulationcraft][:home] = "/opt/simulationcraft"
+default[:simulationcraft][:bin_path] = "/usr/local/bin/simc"
